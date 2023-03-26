8046748166d68e6713c7a2ae0c81dbd88a559376
Samantha Genevay <samanthagenevay@gmail.com>
2023-03-26 12:57:59 -0700

    Completed Lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,25 +1,35 @@
+import ipdb
 from models import Dog
 
-def create_table(base):
-    pass
+spot = Dog()
+
+def create_table(base, engine):
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def new_from_db(session):
     pass
 
 def get_all(session):
-    pass
+    print(session.query(Dog).all())
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name == name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter(Dog.name == name and Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.add(dog)
+    session.commit()
+
+
+ipdb.set_trace()
--- lib/models.py
+++ lib/models.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (PrimaryKeyConstraint, Column, String, Integer)
+from sqlalchemy import (PrimaryKeyConstraint, Column, String, Integer, create_engine)
 from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 class Dog(Base):
-    pass
+    __tablename__ = 'dogs'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    breed = Column(String())
+
+    def __repr__(self):
+        return f"Id: {self.id}, name: {self.name}, breed: {self.breed}"
+
+# if __name__ == '__main__':
+#     engine = create_engine('sqlite:///dogs.db')
+#     Base.metadata.create_all(engine)
+
+#     # use our engine to configure a 'Session' class
+#     Session = sessionmaker(bind=engine)
+#     # use 'Session' class to create 'session' object
+#     session = Session()
